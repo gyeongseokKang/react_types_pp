b919f8420f7ac84ad4a81118644b79fe962c3557
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2021-01-16 19:17:23 +0900

    [Install] 필요한 패키지 설정 및 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from '@material-ui/core';
+import { TopNavigation } from "./page/TopNavigation";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <TopNavigation />
         <div> my new React - typescript portpolio</div>
       </header>
     </div>
---
+++ src/page/TopNavigation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from '@material-ui/core';
+
+
+
+export const TopNavigation = () => {
+    return (
+        <Button color="primary">Hello World</Button>
+    );
+}
+
